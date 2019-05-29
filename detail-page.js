e57e8cb50694ba9f45c87c1b750841280944ab1f
luca2402 <luca.manzone@outlook.com>
2019-05-29 16:24:30 +0200

    Create elements with data from form

--- detail-page.js
+++ detail-page.js
@@ -1,3 +1,18 @@
-function submitComment() {
-    console.log('Hello!')
-  }
+function submitComment(){
+    const inputField = document.getElementById('name')
+    console.log(inputField)
+    const name = inputField.value 
+    console.log(name)
+    const textArea = document.getElementById('msg')
+    console.log(textArea)
+    const msg = textArea.value
+    console.log(msg)
+    const comment = document.createElement('section')
+    const h3 = document.getElementById('h3')
+    const p = document.getElementById('p')
+    h3.innerHTML = `${name} said:`
+    p.innerHTML = msg
+    comment.classList.add('comment')
+    comment.appendChild(h3)
+    comment.appendChild(p)
+}
